8a27be4fdaeecda8ce10b3e15965723a267ecd98
varun-Tad <varuntad141@gmail.com>
2022-05-20 18:26:11 +0530

    added image

--- sidebar/src/pages/Home.js
+++ sidebar/src/pages/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Home() {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <img src="https://static.onecms.io/wp-content/uploads/sites/14/2020/07/15/fashion-header-1-2000.jpg" />
+      Home
+    </div>
+  );
 }
 
 export default Home;
